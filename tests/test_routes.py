24afb1659698d804519e6900425451c7a28dc9ba
Capks <95076320+Capksz@users.noreply.github.com>
2025-03-04 19:41:40 +0000

    Cleaned unnecessary code

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -38,41 +38,6 @@ DATABASE_URI = os.getenv(
 #  T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceService(TestCase):
-    """REST API Server Tests"""
-
-    @classmethod
-    def setUpClass(cls):
-        """Run once before all tests"""
-        app.config["TESTING"] = True
-        app.config["DEBUG"] = False
-        # Set up the test database
-        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-        app.logger.setLevel(logging.CRITICAL)
-        app.app_context().push()
-
-    @classmethod
-    def tearDownClass(cls):
-        """Run once after all tests"""
-        db.session.close()
-
-    def setUp(self):
-        """Runs before each test"""
-        self.client = app.test_client()
-        db.session.query(Shopcart).delete()  # clean up the last tests
-        db.session.commit()
-
-    def tearDown(self):
-        """This runs after each test"""
-        db.session.remove()
-
-    ######################################################################
-    #  P L A C E   T E S T   C A S E S   H E R E
-    ######################################################################
-    def test_index(self):
-        """It should call the home page"""
-        resp = self.client.get("/")
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
 
 class TestShopcartService(TestCase):
@@ -87,6 +52,7 @@ class TestShopcartService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = (
             "postgresql+psycopg2://postgres:postgres@localhost:5432/testdb"
         )
+        app.logger.setLevel(logging.CRITICAL)
         app.app_context().push()
         db.create_all()
 
